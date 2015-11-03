7fa5c9a4bf2b7bd6be7b76e9b63b5961e51f466a
Sal Niro <salvatore.niro@outlook.com>
2015-11-02 17:10:06 -0700

    initial code

---
+++ demo-site/js/app.js
@@ -0,0 +1,5 @@
+﻿var app = angular.module('app', ['ngTick']);
+
+app.controller('ctrl', ['$scope', function ($scope) {
+
+}]);
--- ng-tick/src/ng-tick.js
+++ ng-tick/src/ng-tick.js
@@ -1,1 +1,33 @@
-﻿console.log('init');
+﻿angular.module('ngTick', [])
+.directive('tick', function ($timeout) {
+	return {
+		scope: {
+			start: '=',
+			end: '='
+		},
+		link: function (scope, elem, attrs) {
+
+			var delay = 0;
+
+			$timeout(function () {
+				(function tick(i) {
+
+					function increment(i) {
+						if (++i <= scope.end)
+							tick(i);
+					}
+
+					function decrement(i) {
+						if (--i <= scope.start && --i >= scope.end)
+							tick(i);
+					}
+
+					setTimeout(function () {
+						elem.text(i);
+						scope.start < scope.end ? increment(i) : decrement(i);
+					}, delay);
+				})(scope.start);
+			});
+		}
+	}
+});
