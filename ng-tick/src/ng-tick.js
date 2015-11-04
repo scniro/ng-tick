550093b6f7e761065f016071f0da7a49d1cdc047
Sal Niro <salvatore.niro@outlook.com>
2015-11-03 17:02:40 -0700

    timer work

--- ng-tick/src/ng-tick.js
+++ ng-tick/src/ng-tick.js
@@ -2,7 +2,9 @@
 .directive('tick', function ($filter) {
 	return {
 		scope: {
-			tick: '@'
+			tick: '@',
+			format: '@',
+			from: '@'
 		},
 		link: function (scope, elem, attrs) {
 
@@ -20,18 +22,40 @@
 
 						var date = new Date(start + tick);
 
-						elem.text(filter(new Date(date), 'MMM dd yyyy hh:mm:ss'));
+						scope.format ? elem.text(filter(new Date(date.toUTCString()), scope.format)) : elem.text(new Date(date.toUTCString()));
 					}
 				});
 
 				timer.start();
 			}
 
+			function countdown() {
+
+				function onCountDownEnd() {
+					console.log('end');
+				}
+
+				var timer = new Tock({
+					countdown: true,
+					interval: 1000,
+					callback: function () {
+
+						var round = (Math.ceil(timer.lap() / 1000) * 1000) >= 0 ? Math.ceil(timer.lap() / 1000) * 1000 : 0;
+
+						elem.text(timer.msToTimecode(round));
+					},
+					complete: onCountDownEnd
+				});
+
+				timer.start(scope.from);
+			}
+
 			function ticker() {
 
 				var timer = new Tock({
 					callback: function () {
-						elem.text(timer.lap());
+
+						elem.text(timer.msToTimecode(timer.lap()));
 					}
 				});
 
@@ -41,6 +65,7 @@
 			scope.start = function () {
 				switch (scope.tick.toLowerCase()) {
 					case 'clock': clock(); break;
+					case 'countdown': countdown(); break;
 					case 'ticker': ticker(); break;
 				}
 			}
